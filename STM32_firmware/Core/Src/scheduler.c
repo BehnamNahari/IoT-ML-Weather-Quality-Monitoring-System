4fbfbc68eb75c16b07f0932abc737a20c182c728
BehnamNahari <behnam.nhri@gmail.com>
2026-06-28 00:39:37 +0330

    end

--- STM32_firmware/Core/Src/scheduler.c
+++ STM32_firmware/Core/Src/scheduler.c
@@ -21,34 +21,38 @@ void Scheduler_Init(void)
 
 void Scheduler_Run(void)
 {
-	
-	
-	
-	Fault_Update();
-	CLI_Process();
-	
-	
-	
-	static uint32_t last_sensor_time = 0;
-	static uint32_t last_log_time = 0;
-	
-	uint32_t now = HAL_GetTick();
-	gWeather.timestamp_ms = now;
-	
-	if((now - last_sensor_time) >= SENSOR_PERIOD_MS)
-	{
-		MockSensor_Update();
-		gWeather.sample_id++;
-		Filter_Update();
-		AQI_Update();
-		History_PushSample();
-		Predictor_Update();
-		Alarm_Update();
-		last_sensor_time = now;
-	}
-	if((now - last_log_time) >= LOG_PERIOD_MS)
-	{
-		Logger_Report();
-		last_log_time = now;
-	}
+    static uint32_t last_sensor_time = 0;
+    static uint32_t last_log_time = 0;
+
+    uint32_t now = HAL_GetTick();
+
+    gWeather.timestamp_ms = now;
+
+    CLI_Process();
+
+    if((now - last_sensor_time) >= SENSOR_PERIOD_MS)
+    {
+        MockSensor_Update();
+
+        gWeather.sample_id++;
+
+        Filter_Update();
+        AQI_Update();
+
+        History_PushSample();
+
+        Predictor_Update();
+
+        Alarm_Update();
+
+        Fault_Update();
+
+        last_sensor_time = now;
+    }
+
+    if((now - last_log_time) >= LOG_PERIOD_MS)
+    {
+        Logger_Report();
+        last_log_time = now;
+    }
 }
